b29b0195dc8252dce0ee6438bbb2e5c8ec2c09fa
AkagiShiroe <kirihaasuka@gmail.com>
2021-03-04 10:56:20 +0800

    core_ctl: Add online delay tunable
    Hotplugging is only valuable when cores are offlined for a decent
    amount of time. Constantly plugging and unplugging wastes energy
    turning the cores on and off, as well as cpu cycles. Forcing cores
    offline for a minimum amount of time is a good way to counteract
    this.
    
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- drivers/soc/qcom/core_ctl.c
+++ drivers/soc/qcom/core_ctl.c
@@ -63,6 +63,7 @@ struct cpu_data {
 	unsigned int min_cpus;
 	unsigned int max_cpus;
 	unsigned int offline_delay_ms;
+	unsigned int online_delay_ms;
 	unsigned int busy_up_thres[MAX_CPUS_PER_GROUP];
 	unsigned int busy_down_thres[MAX_CPUS_PER_GROUP];
 	unsigned int online_cpus;
@@ -151,6 +152,20 @@ static ssize_t store_offline_delay_ms(struct cpu_data *state,
 	return count;
 }
 
+static ssize_t store_online_delay_ms(struct cpu_data *state,
+					const char *buf, size_t count)
+{
+	unsigned int val;
+
+	if (kstrtouint(buf, 0, &val))
+		return -EINVAL;
+
+	state->online_delay_ms = val;
+	apply_need(state);
+
+	return count;
+}
+
 static ssize_t show_task_thres(struct cpu_data *state, char *buf)
 {
 	return snprintf(buf, PAGE_SIZE, "%u\n", state->task_thres);
@@ -176,6 +191,11 @@ static ssize_t store_task_thres(struct cpu_data *state,
 static ssize_t show_offline_delay_ms(struct cpu_data *state, char *buf)
 {
 	return snprintf(buf, PAGE_SIZE, "%u\n", state->offline_delay_ms);
+}
+
+static ssize_t show_online_delay_ms(struct cpu_data *state, char *buf)
+{
+	return snprintf(buf, PAGE_SIZE, "%u\n", state->online_delay_ms);
 }
 
 static ssize_t store_busy_up_thres(struct cpu_data *state,
@@ -448,6 +468,7 @@ __ATTR(_name, 0644, show_##_name, store_##_name)
 core_ctl_attr_rw(min_cpus);
 core_ctl_attr_rw(max_cpus);
 core_ctl_attr_rw(offline_delay_ms);
+core_ctl_attr_rw(online_delay_ms);
 core_ctl_attr_rw(busy_up_thres);
 core_ctl_attr_rw(busy_down_thres);
 core_ctl_attr_rw(task_thres);
@@ -464,6 +485,7 @@ static struct attribute *default_attrs[] = {
 	&min_cpus.attr,
 	&max_cpus.attr,
 	&offline_delay_ms.attr,
+	&online_delay_ms.attr,
 	&busy_up_thres.attr,
 	&busy_down_thres.attr,
 	&task_thres.attr,
@@ -531,7 +553,7 @@ static void update_running_avg(bool trigger_update)
 	int cpu;
 	struct cpu_data *pcpu;
 	int avg, iowait_avg, big_avg, old_nrrun;
-	s64 now;
+	s64 now, elapsed;
 	unsigned long flags;
 
 	spin_lock_irqsave(&state_lock, flags);
@@ -653,11 +675,17 @@ static bool eval_need(struct cpu_data *f)
 		spin_unlock_irqrestore(&state_lock, flags);
 		return 0;
 	}
+	
+        elapsed = now - f->need_ts;
 
 	if (need_cpus > last_need) {
-		ret = 1;
+		if (elapsed >= f->online_delay_ms) {
+			ret = 1;
+		} else {
+			mod_timer(&f->timer, jiffies +
+				  msecs_to_jiffies(f->online_delay_ms));
+		}
 	} else if (need_cpus < last_need) {
-		s64 elapsed = now - f->need_ts;
 		if (elapsed >= f->offline_delay_ms) {
 			ret = 1;
 		} else {
@@ -1039,6 +1067,7 @@ static int group_init(struct cpumask *mask)
 	f->need_cpus  = f->num_cpus;
 	f->avail_cpus  = f->num_cpus;
 	f->offline_delay_ms = 100;
+	f->online_delay_ms = 500;
 	f->task_thres = UINT_MAX;
 	f->nrrun = f->num_cpus;
 	INIT_LIST_HEAD(&f->lru);
